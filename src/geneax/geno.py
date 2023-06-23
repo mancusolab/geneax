switching to 64bit precision in mean/scale terms fixed numerics

src/geneax/geno.py
@@ -15,6 +15,9 @@ from jax.experimental import sparse
 from jaxtyping import Array, ArrayLike, Float, Num
 
 
+_sparse_mean = sparse.sparsify(jnp.mean)
+
+
 @jax.jit
 @sparse.sparsify
 def _get_mean_terms(geno: ArrayLike, covar: ArrayLike) -> Array:
@@ -33,24 +36,22 @@ def _get_mean_terms(geno: ArrayLike, covar: ArrayLike) -> Array:
     return beta
 
 
-@jax.jit
-@sparse.sparsify
-def _get_var(geno: sparse.JAXSparse):
+def _get_dense_var(geno: sparse.JAXSparse, dense_dtype: JAXType):
     # def _inner(_, variant):
     #     var_idx = jnp.mean(variant **2) - jnp.mean(variant) ** 2
     #     return _, var_idx
     #
     # _, var_geno = lax.scan(_inner, 0.0, geno.T)
-    var_geno = jnp.mean(geno**2, axis=0) - jnp.mean(geno, axis=0) ** 2
+    var_geno = _sparse_mean(geno**2, axis=0, dtype=dense_dtype) - _sparse_mean(geno, axis=0, dtype=dense_dtype) ** 2
 
-    return var_geno
+    return var_geno.todense()
 
 
 class _SparseMatrixOperator(lx.AbstractLinearOperator):
     matrix: sparse.JAXSparse
     dense_dtype: JAXType
 
-    def __init__(self, matrix, dense_dtype: JAXType = jnp.float32):
+    def __init__(self, matrix, dense_dtype: JAXType = jnp.float64):
         self.matrix = matrix
         self.dense_dtype = dense_dtype
 
@@ -89,14 +90,14 @@ class SparseGenotype(lx.AbstractLinearOperator):
         geno: sparse.JAXSparse,
         covar: Optional[ArrayLike] = None,
         scale: bool = False,
-        dense_dtype: JAXType = jnp.float32,
+        dense_dtype: JAXType = jnp.float64,
     ):
         n, p = geno.shape
         geno_op = _SparseMatrixOperator(geno, dense_dtype)
 
         if covar is None:
             covar = jnp.ones((n, 1), dtype=dense_dtype)
-            beta = sparse.sparsify(jnp.mean)(geno, axis=0).todense().astype(dense_dtype)
+            beta = _sparse_mean(geno, axis=0, dtype=dense_dtype).todense()
             beta = beta.reshape((1, p))
         else:
             beta = _get_mean_terms(geno, covar)
@@ -104,7 +105,7 @@ class SparseGenotype(lx.AbstractLinearOperator):
         center_op = lx.MatrixLinearOperator(covar) @ lx.MatrixLinearOperator(beta)
 
         if scale:
-            wgt = jnp.sqrt(_get_var(geno).todense()).astype(dense_dtype)
+            wgt = jnp.sqrt(_get_dense_var(geno, dense_dtype))
             scale_op = lx.DiagonalLinearOperator(1.0 / wgt)
             self.geno = (geno_op - center_op) @ scale_op
         else:

tests/test_geno.py
@@ -37,10 +37,13 @@ def test_matmul(N: int, P: int, K: int, sp_cls: Type[xsp.JAXSparse], seed: int =
     key, g_key = rdm.split(key)
 
     G = _get_geno(g_key, N, P)
-    Z = (G - jnp.mean(G, axis=0)) / jnp.std(G, axis=0)
+    M = jnp.mean(G, axis=0, dtype=jnp.float64)
+    S = 1.0 / jnp.std(G, axis=0, dtype=jnp.float64)
+    Z = (G - M) * S
     geno = gx.SparseGenotype(
-        sp_cls.fromdense(G).astype(jnp.int8),
+        sp_cls.fromdense(G),
         scale=True,
+        dense_dtype=jnp.float64,
     )
 
     key, r_key = rdm.split(key)
@@ -70,8 +73,14 @@ def test_rmatmul(N: int, P: int, K: int, sp_cls: Type[xsp.JAXSparse], seed: int 
     key, g_key = rdm.split(key)
 
     G = _get_geno(g_key, N, P)
-    Z = (G - jnp.mean(G, axis=0)) / jnp.std(G, axis=0)
-    geno = gx.SparseGenotype(sp_cls.fromdense(G).astype(jnp.int8), scale=True)
+    M = jnp.mean(G, axis=0, dtype=jnp.float64)
+    S = 1.0 / jnp.std(G, axis=0, dtype=jnp.float64)
+    Z = (G - M) * S
+    geno = gx.SparseGenotype(
+        sp_cls.fromdense(G),
+        scale=True,
+        dense_dtype=jnp.float64,
+    )
 
     key, l_key = rdm.split(key)
     L = rdm.normal(l_key, shape=(K, N))